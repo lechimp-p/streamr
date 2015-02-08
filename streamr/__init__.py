1cf03545ba04781691d1db711614843981be3750
Richard Klees <richard.klees@rwth-aachen.de>
2015-02-08 14:36:58 +0100

    imports

--- streamr/__init__.py
+++ streamr/__init__.py
@@ -1,3 +1,6 @@
 # Copyright (C) 2015 Richard Klees <richard.klees@rwth-aachen.de>
 
+# TODO: Clean this up, when user interface is defined.
 from .base import *
+from .lists import *
+from .generators import *
