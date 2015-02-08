79584b545a64a80c36c0ccc80ef681ba990429d9
Richard Klees <richard.klees@rwth-aachen.de>
2015-02-08 15:03:18 +0100

    fixed bugs in list producer/consumer

--- streamr/lists.py
+++ streamr/lists.py
@@ -1,31 +1,42 @@
 # Copyright (C) 2015 Richard Klees <richard.klees@rwth-aachen.de>
 
-from .base import Producer
+from .base import Producer, Consumer, MayContinue
 
 class ListP(Producer):
-    def __init__(self, list):
+    def __init__(self, type, list):
+        self.type = type
         self.list = list
         self.length = len(list)
 
-    def get_inital_state(self):
-        return 0
-    def shutdown_state(self):
+    def type_out(self):
+        return self.type
+
+    def get_initial_state(self):
+        return [0]
+    def shutdown_state(self, state):
         pass
 
     def produce(self, index):
-        return self.list[index]
+        index[0] += 1
+        return self.list[index[0]-1]
     def can_produce(self, index):
-        return index < self.length
+        return index[0] < self.length
 
 class ListC(Consumer):
+    def __init__(self, type):
+        self.type = type
+
+    def type_in(self):
+        return self.type
+
     def get_initial_state(self):
         return []
-    def shutdown_state(self):
+    def shutdown_state(self, state):
         pass
 
     def consume(self, value, list):
         list.append(value)
-    def can_consume(self, list):
+    def can_continue(self, list):
         return MayContinue;
     def result(self, list):
         return list
